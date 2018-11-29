78bc69ee8e7602c0fc297c6e9a3fcefa4c66eb94
harishdadi <harish.dadi@tatahealth.com>
2018-11-29 11:56:08 +0530

    Styling

--- src/client/containers/App.js
+++ src/client/containers/App.js
@@ -10,44 +10,42 @@ class App extends Component {
       const ques = this.props.questions[id - 1];
       const answer = ques.userAnswer;
       return (
-        <div className="container">
+        <div className="App">
           <h1 style={{ fontSize: 150 }}>Quiz</h1>
-          <div className="App">
-            <p>
-              {ques.id + " "}
-              {ques.question}
-            </p>
-            <form>
-              <input
-                type="radio"
-                name="group"
-                value={ques.options[0]}
-                checked={answer === 1}
-                onChange={() => this.props.markAnswerQuestion(1, id)}
-              />
-              {ques.options[0]}
-              <br />
-              <br />
-              <input
-                type="radio"
-                name="group"
-                value={ques.options[1]}
-                checked={answer === 2}
-                onChange={() => this.props.markAnswerQuestion(2, id)}
-              />
-              {ques.options[1]}
-              <br />
-              <br />
-              <input
-                type="radio"
-                name="group"
-                value={ques.options[2]}
-                checked={answer === 3}
-                onChange={() => this.props.markAnswerQuestion(3, id)}
-              />
-              {ques.options[2]} <br />
-            </form>
-          </div>
+          <h3>
+            {ques.id + " "}
+            {ques.question}
+          </h3>
+          <form>
+            <input
+              type="radio"
+              name="group"
+              value={ques.options[0]}
+              checked={answer === 1}
+              onChange={() => this.props.markAnswerQuestion(1, id)}
+            />
+            {ques.options[0]}
+            <br />
+            <br />
+            <input
+              type="radio"
+              name="group"
+              value={ques.options[1]}
+              checked={answer === 2}
+              onChange={() => this.props.markAnswerQuestion(2, id)}
+            />
+            {ques.options[1]}
+            <br />
+            <br />
+            <input
+              type="radio"
+              name="group"
+              value={ques.options[2]}
+              checked={answer === 3}
+              onChange={() => this.props.markAnswerQuestion(3, id)}
+            />
+            {ques.options[2]} <br />
+          </form>
         </div>
       );
     } else {
